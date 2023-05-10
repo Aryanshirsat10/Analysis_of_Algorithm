1fbd2ced5fcaf49d256e73551bb4064c12a12671
Aryan Padmakar Shirsat <99379971+Aryanshirsat10@users.noreply.github.com>
2023-05-10 11:06:24 +0530

    Create knapsack1.java

---
+++ GreedyAlgorithms/knapsack1.java
@@ -0,0 +1,61 @@
+package GreedyAlgorithms;
+
+import java.util.*;
+class knapsack{
+void knapsack3(int n,int w,int wt[],int p[]){
+double r[]= new double[n];
+double temp;
+double maxval=0;
+int balance=w;
+for(int i=1;i<n;i++){
+r[i]=(double)p[i]/wt[i];
+}
+for(int i=0;i<n-1;i++){
+for(int j=n+1;j<n;j++){
+if(r[i]<r[j]){
+temp=r[i];
+r[i]=r[j];
+r[j]=temp;
+int temp1=wt[i];
+wt[i]=wt[j];
+wt[j]=temp1;
+int temp2 = p[i];
+p[i]=p[j];
+p[j]=temp2;
+}
+}
+}
+for(int i=0;i<n;i++){
+if(wt[i]<=balance){
+maxval=maxval+p[i];
+balance= balance-wt[i];
+}
+else{
+maxval=maxval+r[i]*balance;
+}
+}
+System.out.println("Final Profit is: "+maxval);
+}
+}
+public class knapsack1{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+knapsack kp = new knapsack();
+System.out.println("Enter the number of items");
+int n = sc.nextInt();
+System.out.println("Enter the sack size:");
+int w = sc.nextInt();
+int wt[]= new int[n];
+int p[] = new int [n];
+System.out.println("Enter the weight of "+n+" items");
+for (int i = 0; i < n; i++) {
+wt[i]=sc.nextInt();
+}
+System.out.println("Enter the profit of "+n+" items");
+for(int i=0;i<n;i++){
+p[i]=sc.nextInt();
+}
+kp.knapsack3(n, w, wt, p);
+sc.close();
+}
+}
