9da76ad1e9dfc481b17285c549308e0b449b25f5
Aryan Padmakar Shirsat <99379971+Aryanshirsat10@users.noreply.github.com>
2023-05-11 19:50:01 +0530

    Update Apsp.java

--- DynamicProgramming/Apsp.java
+++ DynamicProgramming/Apsp.java
@@ -1,48 +1,58 @@
 package DynamicProgramming;
+import java.util.Scanner;
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;;
-class Allpairsp{
-final static int INF = 99999, V = 3;
-void floydWarshall(int dist[][])
-{
-int i, j, k;
-for (k = 0; k < V; k++) {
-for (i = 0; i < V; i++) {
-for (j = 0; j < V; j++) {
-    if (dist[i][k] + dist[k][j]
-< dist[i][j])
-dist[i][j]
-= dist[i][k] + dist[k][j];
-}
-}
-}
-printSolution(dist);
-}
-void printSolution(int dist[][])
-{
-System.out.println(
-"The following matrix shows the shortest "
-+ "distances between every pair of vertices");
-for (int i = 0; i < V; ++i) {
-for (int j = 0; j < V; ++j) {
-if (dist[i][j] == INF)
-System.out.print("INF ");
-else
-System.out.print(dist[i][j] + " ");
-}
-System.out.println();
-}
-}
-}
-class Apsp {
-public static void main(String[] args) {
-int graph[][]= {{0,4,11},
-{6,0,2},
-{3,Allpairsp.INF,0}
-};
-Allpairsp a = new Allpairsp();
-a.floydWarshall(graph);
-}
+public class Apsp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of vertices: ");
+        int n = sc.nextInt();
+
+        int[][] graph = new int[n][n];
+
+        System.out.println("Enter the adjacency matrix: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = sc.nextInt();
+            }
+        }
+
+        floydWarshall(graph, n);
+
+        sc.close();
+    }
+
+    public static void floydWarshall(int[][] graph, int n) {
+        int[][] dist = new int[n][n];
+
+        // Initialize dist array with the values from the graph matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = graph[i][j];
+            }
+        }
+
+        // Compute all pairs shortest path
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE && dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        // Print the shortest path matrix
+        System.out.println("The all pairs shortest path matrix is:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][j] == Integer.MAX_VALUE) {
+                    System.out.print("INF ");
+                } else {
+                    System.out.print(dist[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
