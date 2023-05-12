6a2dac892164f402e1bfe5f71bd9d8247d31db98
Aryan Padmakar Shirsat <99379971+Aryanshirsat10@users.noreply.github.com>
2023-05-12 10:17:13 +0530

    Update MatricChainMultiplication.java

--- DynamicProgramming/MatricChainMultiplication.java
+++ DynamicProgramming/MatricChainMultiplication.java
@@ -1,32 +1,39 @@
 package DynamicProgramming;
 
 import java.util.*;
+
 class MatrixChainMultiplication {
-static int MatrixChainOrder(int p[], int i, int j)
-{
-if (i == j)
-return 0;
-int min = Integer.MAX_VALUE;
-for (int k = i; k < j; k++) {
-int count = MatrixChainOrder(p, i, k)
-+ MatrixChainOrder(p, k + 1, j)
-+ p[i - 1] * p[k] * p[j];
-if (count < min)
-min = count;
-}
-return min;
-}
-public static void main(String args[])
-{
-Scanner sc = new Scanner(System.in);
-int N = sc.nextInt();
-int arr[] = new int[N];
-for(int i=0; i<N; i++){
-arr[i]=sc.nextInt();
-}
-System.out.println(
-"Minimum number of multiplications is "
-+ MatrixChainOrder(arr, 1, N - 1));
-sc.close();
- }
+
+    static int MatrixChainOrder(int p[], int n) {
+
+        int m[][] = new int[n][n];
+
+        for (int len = 2; len < n; len++) {
+            for (int i = 1; i < n - len + 1; i++) {
+                int j = i + len - 1;
+                m[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k <= j - 1; k++) {
+                    int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                    if (q < m[i][j]) {
+                        m[i][j] = q;
+                    }
+                }
+            }
+        }
+        return m[1][n - 1];
+    }
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Minimum number of multiplications is " + MatrixChainOrder(arr, n));
+        sc.close();
+    }
 }
